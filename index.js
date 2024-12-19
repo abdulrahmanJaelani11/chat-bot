371783591dc78a1945aec5318ac895e50e9dbfb1
DESKTOP-JHB7MEC\Workplus <randikaangga9044@gmail.com>
2024-12-19 19:17:47 +0700

    ganti ai

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const axios = require("axios");
 const app = express();
 app.use(bodyParser.json());
 
-async function sendFonnte(sender, message) {
+  async function sendFonnte(sender, message) {
    const response = await axios.post(
         "https://api.fonnte.com/send",
         {
@@ -23,59 +23,50 @@ async function sendFonnte(sender, message) {
     console.log(response.data);
   }
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+  async function getDataInstagram(username) {
+    const options = {
+      method: 'GET',
+      url: 'https://instagram-scraper-api2.p.rapidapi.com/v1/info',
+      params: {
+        username_or_id_or_url: 'abdurahman_jaelani'
+      },
+      headers: {
+        'x-rapidapi-key': 'c3a2e5848cmshcac1835d7465444p179415jsn8ca3f96bdb4a',
+        'x-rapidapi-host': 'instagram-scraper-api2.p.rapidapi.com'
+      }
+    };
 
-app.get("/webhook", (req, res) => {
-  res.send("Hello Fonnte!");
-});
-// Endpoint untuk menerima pesan dari WhatsApp API
-app.post("/webhook", async (req, res) => {
-  const { message, sender } = req.body; // Data dari WhatsApp API
-  
-  const options = {
-    method: 'POST',
-    url: 'https://chatgpt-42.p.rapidapi.com/gpt4',
-    headers: {
-      'x-rapidapi-key': 'c3a2e5848cmshcac1835d7465444p179415jsn8ca3f96bdb4a',
-      'x-rapidapi-host': 'chatgpt-42.p.rapidapi.com',
-      'Content-Type': 'application/json'
-    },
-    data: {
-      messages: [
-        {
-          role: 'user',
-          content: `Dalam konteks ini nama kamu Bobi, nama aku Ivi dan Abdurahman bos kamu. Respon aku selaknya manusia berdialog!. "${message}"`
-        }
-      ],
-      web_access: false
+    try {
+      const response = await axios.request(options);
+      return response.data.data;
+    } catch (error) {
+      console.error(error);
     }
-  };
+  }
 
-  // const options = {
-  //   method: 'POST',
-  //   url: 'https://chatgpt-vision1.p.rapidapi.com/gpt4',
-  //   headers: {
-  //     'x-rapidapi-key': 'c3a2e5848cmshcac1835d7465444p179415jsn8ca3f96bdb4a',
-  //     'x-rapidapi-host': 'chatgpt-vision1.p.rapidapi.com',
-  //     'Content-Type': 'application/json'
-  //   },
-  //   data: {
-  //     messages: [
-  //       {
-  //         role: 'user',
-  //         content: 'hi'
-  //       }
-  //     ],
-  //     web_access: false
-  //   }
-  // };
-  
-  try {
-    // Kirim pesan ke OpenAI API
+  async function checkMessage(data) {
+    const {sender, message} = data;
+    const options = {
+      method: 'POST',
+      url: 'https://chatgpt-vision1.p.rapidapi.com/gpt4',
+      headers: {
+        'x-rapidapi-key': 'c3a2e5848cmshcac1835d7465444p179415jsn8ca3f96bdb4a',
+        'x-rapidapi-host': 'chatgpt-vision1.p.rapidapi.com',
+        'Content-Type': 'application/json'
+      },
+      data: {
+        messages: [
+          {
+            role: 'user',
+            content: `Dalam konteks ini nama kamu Bobi, nama aku Ivi dan Abdurahman bos kamu. Respon aku selaknya manusia berdialog!. "${message}"`
+          }
+        ],
+        web_access: false
+      }
+    };
+    
     let reply = "";
-    console.log(sender);
+    
     if(sender === "6285952403737" || sender === "6283874809704"){
       if(message.includes("knp ga mau respon") || message.includes("kenapa ga mau respon") || message.includes("kenapa ga mau jawab") || message.includes("knp ga mau jawab") || message.includes("kenapa ga mau balas") || message.includes("knp ga mau balas") || message.includes("kenapa ga mau chat") || message.includes("knp ga mau chat") || message.includes("kenapa ga mau ngomong") || message.includes("knp ga mau ngomong") || message.includes("kenapa ga mau ngobrol") || message.includes("knp ga mau ngobrol") || message.includes("kenapa ga mau bicara") || message.includes("knp ga mau bicara") || message.includes("kenapa ga mau berbicara") || message.includes("knp ga mau berbicara")){
         reply = "Karena dia nyebelin, aku lebih baik berbicara denganmu saja Ivi!";
@@ -86,9 +77,29 @@ app.post("/webhook", async (req, res) => {
     }else{
       reply = "Maaf, aku hanya mau berbicara dengan teman baruku saja, namanya Silvi";
     }
-    // console.log(reply);
 
-    await sendFonnte(sender, reply); // Kirim balasan ke pengirim pesan
+    return reply;
+  }
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/webhook", (req, res) => {
+  res.send("Hello Fonnte!");
+});
+// Endpoint untuk menerima pesan dari WhatsApp API
+app.post("/webhook", async (req, res) => {
+  const { message, sender } = req.body; // Data dari WhatsApp API
+  try {
+    if(message.includes("ig:")){
+      const username = message.split(":")[1];
+      const data = await getDataInstagram(username);
+      await sendFonnte(sender, JSON.stringify(data));
+    }else{
+      const reply = await checkMessage({sender, message});
+      await sendFonnte(sender, reply); // Kirim balasan ke pengirim pesan
+    }
   } catch (error) {
     await sendFonnte(sender, "Maaf, Sepertinya Server nya ada masalah, silahkan coba lagi!"); // Kirim pesan kesalahan
     res.status(500).send({
